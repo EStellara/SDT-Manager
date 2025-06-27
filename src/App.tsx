031fad595325e68f1601b9fa592d182541b50783
Evelyn Stellara <59183106+EStellara@users.noreply.github.com>
2025-06-27 13:31:23 -0400

    Fixed rendering bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { DialogProjectProvider } from "@/contexts/DialogProjectContext";
 import { HomePage, DialogProjectPage, NotFoundPage } from "@/pages";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/project" element={<DialogProjectPage />} />
-				<Route path="*" element={<NotFoundPage />} />
-			</Routes>
-		</Router>
+		<DialogProjectProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/project" element={<DialogProjectPage />} />
+					<Route path="*" element={<NotFoundPage />} />
+				</Routes>
+			</Router>
+		</DialogProjectProvider>
 	);
 }
 
